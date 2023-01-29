359a50402e82128e9da28c5e4b3c66a1f167250d
mayankverma786 <mayankvverma786@gmail.com>
2023-01-29 11:31:41 +0530

    finished project

---
+++ app/src/main/java/com/example/nearbyhospitals/MapsActivity.java
@@ -0,0 +1,51 @@
+package com.example.nearbyhospitals;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
+{
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng knplating = new LatLng(26.469409, 80.264288);
+        MarkerOptions markerOptions = new MarkerOptions().position(knplating).title("kanpur");
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(knplating, 16f));
+        googleMap.addMarker(new MarkerOptions().position(knplating).title("Marker in Kanpur"));
+
+        mMap.addCircle(new CircleOptions().center(knplating).radius(1000).fillColor(Color.GREEN)
+                .strokeColor(Color.DKGRAY));
+
+    }
+}
